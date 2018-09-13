8383136ccfa7358193f3cc5ecb065324f2365507
eureka <eureka@PC11607028.local>
2018-09-13 19:32:08 +0900

    APIのGetIdealTypeを実装

--- controllers/idealtype/ideal_type.go
+++ controllers/idealtype/ideal_type.go
@@ -2,7 +2,6 @@ package idealtype
 
 import (
 	"github.com/eure/si2018-second-half-1/libs/token"
-	"github.com/eure/si2018-second-half-1/models"
 	"github.com/eure/si2018-second-half-1/repositories"
 	si "github.com/eure/si2018-second-half-1/restapi/summerintern"
 	"github.com/go-openapi/runtime/middleware"
@@ -33,7 +32,23 @@ func GetIdealType(p si.GetIdealTypeParams) middleware.Responder {
 				Message: "Unauthorized :: 無効なトークン",
 			})
 	}
-	if me.ID%2 == 1 {
+	// if me.ID%2 == 1 {
+	// 	return si.NewGetIdealTypeNotFound().WithPayload(
+	// 		&si.GetIdealTypeNotFoundBody{
+	// 			Code:    "404",
+	// 			Message: "Ideal Type Not Found",
+	// 		})
+	// }
+	sr := repositories.NewUserStatsRepository(s)
+	stat, err := sr.GetByUserID(me.ID)
+	if err != nil {
+		return si.NewGetIdealTypeInternalServerError().WithPayload(
+			&si.GetIdealTypeInternalServerErrorBody{
+				Code:    "500",
+				Message: "Internal Server Error :: Statsの取得に失敗しました",
+			})
+	}
+	if stat == nil {
 		return si.NewGetIdealTypeNotFound().WithPayload(
 			&si.GetIdealTypeNotFoundBody{
 				Code:    "404",
@@ -42,30 +57,25 @@ func GetIdealType(p si.GetIdealTypeParams) middleware.Responder {
 	}
 
 	// ダミーデータ
-	var ideal models.IdealType
-	ideal.Age = new(models.IdealTypeAge)
-	ideal.Age.From = "20歳"
-	ideal.Age.To = "25歳"
-	ideal.AnnualIncome = new(models.IdealTypeAnnualIncome)
-	ideal.AnnualIncome.From = "200万円"
-	ideal.AnnualIncome.To = "600万円"
-	ideal.BodyBuild = []string{"スリム", "やや細め", "普通"}
-	ideal.Drinking = []string{"飲む", "ときどき飲む"}
-	ideal.Education = []string{"高校卒", "大学卒"}
-	ideal.Height = new(models.IdealTypeHeight)
-	ideal.Height.From = "165cm"
-	ideal.Height.To = "185cm"
-	ideal.Holiday = []string{"土日", "平日"}
-	ideal.HomeState = []string{"東京", "千葉", "神奈川", "埼玉"}
-	ideal.Job = []string{"会社員", "医師", "弁護士"}
-	ideal.ResidenceState = []string{"東京", "神奈川"}
-	ideal.Smoking = []string{"吸う", "ときどき吸う", "非喫煙者の前では吸わない", "相手が嫌ならやめる", "吸う(電子タバコ)"}
+	// var ideal models.IdealType
+	// ideal.Age = new(models.IdealTypeAge)
+	// ideal.Age.From = "20歳"
+	// ideal.Age.To = "25歳"
+	// ideal.AnnualIncome = new(models.IdealTypeAnnualIncome)
+	// ideal.AnnualIncome.From = "200万円"
+	// ideal.AnnualIncome.To = "600万円"
+	// ideal.BodyBuild = []string{"スリム", "やや細め", "普通"}
+	// ideal.Drinking = []string{"飲む", "ときどき飲む"}
+	// ideal.Education = []string{"高校卒", "大学卒"}
+	// ideal.Height = new(models.IdealTypeHeight)
+	// ideal.Height.From = "165cm"
+	// ideal.Height.To = "185cm"
+	// ideal.Holiday = []string{"土日", "平日"}
+	// ideal.HomeState = []string{"東京", "千葉", "神奈川", "埼玉"}
+	// ideal.Job = []string{"会社員", "医師", "弁護士"}
+	// ideal.ResidenceState = []string{"東京", "神奈川"}
+	// ideal.Smoking = []string{"吸う", "ときどき吸う", "非喫煙者の前では吸わない", "相手が嫌ならやめる", "吸う(電子タバコ)"}
 
-	/*
-		Likeの統計データの取得
-		平均値が格納されたDBから理想のお相手を取得します。
-	*/
-
-	//sEnt := user.Build()
-	return si.NewGetIdealTypeOK().WithPayload(&ideal)
+	sEnt := stat.Build()
+	return si.NewGetIdealTypeOK().WithPayload(&sEnt)
 }
