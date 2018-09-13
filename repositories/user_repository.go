8040859ceb644f16640016c72fa7468b48b042f8
tnkTaka <taka.abc.hiko@gmail.com>
2018-09-13 20:37:42 +0900

    importが循環していたためRe関数の位置を変えた

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -5,8 +5,10 @@ import (
 
 	"github.com/go-openapi/strfmt"
 
+	"strconv"
+	"strings"
+
 	"github.com/eure/si2018-second-half-1/entities"
-	"github.com/eure/si2018-second-half-1/libs/user"
 	"github.com/eure/si2018-second-half-1/models"
 )
 
@@ -68,7 +70,7 @@ func (r *UserRepository) FindWithCondition(limit, offset int, gender string, ids
 
 	// 年収の挿入
 	if searchCondition.AnnualIncome.From != "" || searchCondition.AnnualIncome.To != "" {
-		s.In("", user.ReAnnualIncome(searchCondition.Age.From, searchCondition.Age.To))
+		s.In("", ReAnnualIncome(searchCondition.Age.From, searchCondition.Age.To))
 	}
 
 	// 体型の挿入
@@ -88,7 +90,7 @@ func (r *UserRepository) FindWithCondition(limit, offset int, gender string, ids
 
 	// 身長の挿入
 	if searchCondition.Height.From != "" || searchCondition.Height.To != "" {
-		s.In("", user.ReAnnualIncome(searchCondition.Height.From, searchCondition.Age.To))
+		s.In("", ReHeight(searchCondition.Height.From, searchCondition.Height.To))
 	}
 
 	// 休日の挿入
@@ -119,11 +121,11 @@ func (r *UserRepository) FindWithCondition(limit, offset int, gender string, ids
 	// 年齢の挿入
 	if searchCondition.Age.From != "" || searchCondition.Age.To != "" {
 		if searchCondition.Age.From != "" {
-			s.And("birthday < ?", user.ReAge(searchCondition.Age.From))
+			s.And("birthday < ?", ReAge(searchCondition.Age.From))
 		}
 
 		if searchCondition.Age.To != "" {
-			s.And("birthday > ?", user.ReAge(searchCondition.Age.To))
+			s.And("birthday > ?", ReAge(searchCondition.Age.To))
 		}
 	}
 
@@ -147,4 +149,76 @@ func (r *UserRepository) FindByIDs(ids []int64) ([]entities.User, error) {
 	}
 
 	return users, nil
+}
+
+func ReHeight(from, to string) []string {
+	var heights []string
+	f, _ := strconv.Atoi(strings.Replace(from, "cm", "", -1))
+	t, _ := strconv.Atoi(strings.Replace(to, "cm", "", -1))
+
+	rs := t - f
+
+	heights = append(heights, from)
+	for rs > 0 {
+		f++
+		heights = append(heights, strconv.Itoa(f)+"cm")
+		rs--
+	}
+
+	return heights
+}
+
+func ReAge(age string) time.Time {
+	now := time.Now()
+	a, _ := strconv.Atoi(strings.Replace(age, "歳", "", -1))
+
+	ageTime := now.AddDate(-a, 0, 0)
+	return ageTime
+}
+
+func ReAnnualIncome(from, to string) []string {
+	var annualIncomes []string
+	var f, t int
+
+	f, _ = strconv.Atoi(strings.Replace(from, "万円", "", -1))
+	t, _ = strconv.Atoi(strings.Replace(to, "万円", "", -1))
+
+	if from == "" {
+		f = 200
+		annualIncomes = append(annualIncomes, strconv.Itoa(f)+"万円未満")
+	}
+
+	if to == "" {
+		t = 3000
+	}
+
+	i := f
+
+	for i < t {
+		switch {
+		case i >= 2000:
+			i += 1000
+		case i >= 1000:
+			i += 500
+		case i < 1000:
+			i += 200
+		}
+
+		annualIncomes = append(annualIncomes, strconv.Itoa(f)+"万円以上"+"〜"+strconv.Itoa(i)+"万円未満")
+
+		switch {
+		case f >= 2000:
+			f += 1000
+		case f >= 1000:
+			f += 500
+		case f < 1000:
+			f += 200
+		}
+	}
+
+	if to == "" {
+		annualIncomes = append(annualIncomes, strconv.Itoa(t)+"万円以上")
+	}
+
+	return annualIncomes
 }
